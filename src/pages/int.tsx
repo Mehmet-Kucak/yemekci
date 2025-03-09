c70395fe8b6c565d211e2486f948caca32582be9
Mehmet-Kucak <89465544+Mehmet-Kucak@users.noreply.github.com>
2025-03-09 13:44:00 +0300

    Update int.tsx

--- src/pages/int.tsx
+++ src/pages/int.tsx
@@ -49,7 +49,7 @@ const Int = () => {
           <img src="/back_icon.png" alt="" />
         </button>
         {/* <img src="/YemekCi.png" alt="yemekci" className={styles.logo} /> */}
-        <h2>Gastro Keşif</h2>
+        <h2>GastroKeşif</h2>
         <button className={styles.header_button} onClick={profileButton}>
           <img src="/person_icon.png" alt="" />
         </button>
