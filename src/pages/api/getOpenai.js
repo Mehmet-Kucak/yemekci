4a3fe00a276759e57569cff77367bb613b3b7f0b
Mehmet-Kucak <kucakmehmet2019@gmail.com>
2025-04-18 23:59:34 +0300

    improved the prompt

--- src/pages/api/getOpenai.js
+++ src/pages/api/getOpenai.js
@@ -15,12 +15,48 @@ export default async function handler(req, res) {
 
   try {
     const response = await openai.responses.create({
-      model: "gpt-4.1",
-      tools: [{
-        type: "web_search_preview",
-      }],
-      input: "Verilen coğrafi işaretli yemek hakkında sadece yemekle ilgili tek paragraflık bir açıklama oluştur. Açıklama, yemeğin tarihi, arka planı, kullanılan malzemeler ve özelliklerini içermeli. Cevap sadece belirtilen dilde olmalı, belirtilen şehir dışında başka bor şehirden bahsetme, fazladan bir giriş veya ek açıklama bulunmamalıdır. Yemek:"+name + ", dil:" + lang + " şehir:" + city
-    });
+    model: "gpt-4.1",
+    input: [
+      {
+        "role": "system",
+        "content": [
+          {
+            "type": "input_text",
+            "text": "Write a single-paragraph explanation about the given geographically marked Turkish dish, including its history, background, ingredients, and features. The explanation must be in the specified language, solely about the specified city, without mentioning other cities, extra introductions, or additional explanations."
+          }
+        ]
+      },
+      {
+        "role": "user",
+        "content": [
+          {
+            "type": "input_text",
+            "text": "dish name: " + name + ", city: " + city + ", language: " + lang
+          }
+        ]
+      }
+    ],
+    text: {
+      "format": {
+        "type": "text"
+      }
+    },
+    tools: [
+      {
+        "type": "web_search_preview",
+        "user_location": {
+          "type": "approximate",
+          "country": "TR",
+          "region": city,
+          "city": city
+        },
+        "search_context_size": "medium"
+      }
+    ],
+    temperature: 1,
+    max_output_tokens: 2048,
+    top_p: 1,
+  });
     res.status(200).json({ description: response.output_text });
   } catch (err) {
     console.error("OpenAI hata:", err);
